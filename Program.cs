69bb1f20e168e64a0b30534319bb03b512fe8ac3
Raphael Nascimento <raphael.ofc11@gmail.com>
2024-02-02 12:23:11 -0300

    Finalizando desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new("123456789", "Nokia 3310", "123456789", 16);
+Iphone iphone = new("987654321", "Iphone 12", "987654321", 128);
+
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("WhatsApp");
+
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("WhatsApp");
